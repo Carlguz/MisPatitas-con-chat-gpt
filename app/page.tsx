3ea43ed3473ebe81f724b0d56b0384967f8f3037
Carlguz <71299737+Carlguz@users.noreply.github.com>
2025-08-09 16:30:58 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{minHeight:'100vh',display:'grid',placeItems:'center',fontFamily:'sans-serif'}}>
+      <h1>Hola, Mis Patitas 🐾</h1>
+    </main>
+  );
+}
