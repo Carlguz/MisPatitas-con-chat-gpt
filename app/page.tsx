568488aa262566793cfafda4dcf38c347afb4655
Carlguz <71299737+Carlguz@users.noreply.github.com>
2025-08-09 16:27:50 -0500

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{minHeight:'100vh',display:'grid',placeItems:'center',fontFamily:'sans-serif'}}>
-      <h1>Hola, Mis Patitas 🐾</h1>
-    </main>
-  );
-}
